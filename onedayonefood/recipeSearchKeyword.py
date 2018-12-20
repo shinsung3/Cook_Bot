781166358fe7430716316486dad376f157f4fed6
ldh5624 <ldh5624@gmail.com>
2018-12-20 14:04:29 +0900

    만개의 레시피 키워드 검색 후 디테일 레시피 챗봇이 보이게, 냉장고를 부탁해 파일 만듬 현재 진행중

--- onedayonefood/recipeSearchKeyword.py
+++ onedayonefood/recipeSearchKeyword.py
@@ -19,27 +19,34 @@ sc = SlackClient(slack_token)
 
 driver = webdriver.Chrome(r'C:\Users\student\Desktop\chromedriver.exe')
 
+
 # 세부 레시피 크롤링
 def crawl_detail_recipe(text):
-    url = "http://tv.jtbc.joins.com/photo/pr10010331/pm10026814"
-    driver.get(url)
+    #  open website
+    driver.get("http://www.10000recipe.com/recipe/list.html")
+    # 검색창에 검색
+    search_text = driver.find_element_by_id("srhRecipeText")
+    text = re.sub(r'<@\S+> ', '', text)
 
-    if "1" in url:
-        source = driver.page_source
-        soup = BeautifulSoup(source, "html.parser")
+    search_text.send_keys(text)  # text
 
-        keywords = []
+    search_btn = driver.find_element_by_css_selector("button.btn.btn-default")
+    search_btn.click()
 
-        chefs = soup.find_all("span", class_="txt")
-        for i in range(9):
-            keywords.append(str(i+1)+"위 : "+chefs[i].get_text().strip())
+    bt = driver.find_element_by_class_name("thumbnail")
+    bt.click()
 
+    # 엘리스 url
+    # url = "http://www.10000recipe.com/recipe/6853017"
+    # source = urllib.request.urlopen(url).read()
+    source = driver.page_source
+    soup = BeautifulSoup(source, "html.parser")
 
-        contents = soup.find("div", class_="view_cont_txt").get_text().strip()
-        # contents.find("script").extract()
-        # contents = contents.get_text().strip()
-    else:
-        pass
+    contents = soup.find("div", class_="view_step")
+    contents.find("script")
+    if contents.find("script"):
+        contents.find("script").extract()
+    contents = contents.get_text().strip()
 
     return contents
 
@@ -97,4 +104,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8080)
+    app.run('127.0.0.1', port=5000)
